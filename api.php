8e3de300de689fe92130f377fe13592850a3ea77
mkhizeryounas <m.khizeryounas@gmail.com>
2018-04-22 16:47:33 +0500

    OK

--- api.php
+++ api.php
@@ -1,9 +1,11 @@
 <?php
 header("Content-Type: application/json");
 $var = file_get_contents("https://api.apify.com/v1/execs/xarPDpN8g54y9NbZh/results?format=json&simplified=1");
+$_POST = json_decode(file_get_contents('php://input'), true);
 
 $var = json_decode($var, true);
 $res = [];
+
 foreach($var as $e)  {
     if(
         strpos($e['title'], $_GET['size']) &&
@@ -14,4 +16,11 @@ foreach($var as $e)  {
     }
 }
 
-echo json_encode($res);
+$response = [
+    "speech" => "Dev server has received the request",
+    "displayText" => "Dev server has received the request",
+    "data" => $_POST,
+    "source" => "dev_server"
+];
+
+echo json_encode($response);
